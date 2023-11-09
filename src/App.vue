2cf663c7656ecec3884a0a36e0b62332ffd2b0f0
giuliabaratella <giuliabaratella7@gmail.com>
2023-11-09 15:17:09 +0100

    added header,main,footer components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-
-  </div>
+  <HeaderComponent/>
+  <MainComponent/>
+  <FooterComponent/>
 </template>
 
 <script>
+import MainComponent from './components/MainComponent.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
   export default {
-    
+    components:{
+      HeaderComponent,
+      MainComponent,
+      FooterComponent,
+
+    }
   }
 </script>
 
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,15 @@
+<template>
+<footer>
+    footer
+</footer>
+</template>
+
+<script>
+    export default {
+        name: 'FooterComponent',
+    }
+</script>
+
+<style scoped>
+
+</style>
