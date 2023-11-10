2798c58ba01033b50ee8eba5c268c72177f94e88
giuliabaratella <giuliabaratella7@gmail.com>
2023-11-10 15:03:05 +0100

    updated footerSocial + socialIcons + img

--- src/components/FooterSocial.vue
+++ src/components/FooterSocial.vue
@@ -2,21 +2,18 @@
     <div class="footer-social">
         <div class="container d-flex justify-content-between py-5">
             <button class="btn text-uppercase fw-bold">Sign-up now!</button>
-            <div class="d-flex column-gap-3 align-items-center">
-                <div class="text-uppercase fw-bold follow-us">Follow us</div>
-                <a href="#" class="d-flex justify-content-center align-items-center" @click.prevent>F</a>
-                <a href="#" class="d-flex justify-content-center align-items-center" @click.prevent>T</a>
-                <a href="#" class="d-flex justify-content-center align-items-center" @click.prevent>Y</a>
-                <a href="#" class="d-flex justify-content-center align-items-center" @click.prevent>P</a>
-                <a href="#" class="d-flex justify-content-center align-items-center" @click.prevent>G</a>
-            </div>
+            <SocialIcons/>
         </div>
     </div>
 </template>
 
 <script>
+import SocialIcons from './SocialIcons.vue'
     export default {
         name:'FooterSocial',
+        components:{
+            SocialIcons,
+        }
 
     }
 </script>
@@ -27,18 +24,6 @@
         button {
             color: var(--color-light);
             border: 2px solid var(--color-primary);
-        }
-        .follow-us{
-            color: var(--color-primary);
-            font-size: 1.5em;
-        }
-        a{
-            width: 40px;
-            height: 40px;
-            padding: 10px;
-            border-radius: 50%;
-            background-color: var(--color-icons);
-            color: var(--color-dark);
         }
     }
 
---
+++ src/components/SocialIcons.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="d-flex column-gap-3 align-items-center">
+        <div class="text-uppercase fw-bold follow-us">Follow us</div>
+        <div class="d-flex column-gap-3 align-items-center ">
+            <a href="#" @click.prevent v-for="el in socialIcons">
+                <img :src=el alt="social media link">
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'SocialIcons',
+        data(){
+            return{
+                socialIcons:[
+                    '/img/footer-fb.png',
+                    '/img/footer-twit.png',
+                    '/img/footer-youtube.png',
+                    '/img/footer-pinterest.png',
+                    '/img/footer-periscope.png',
+                    ]
+                }
+
+            }
+    }
+</script>
+
+<style scoped lang="scss">
+.follow-us{
+            color: var(--color-primary);
+            font-size: 1.5em;
+            
+        }
+    a img:hover{
+            opacity: 80%;
+        }
+</style>
