f858ed92f99f4c2310c96cb3131a34a0d5676378
giuliabaratella <giuliabaratella7@gmail.com>
2023-11-10 18:19:08 +0100

    added main content (prop card)

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card-comics">
+        <div class="img-box mb-3">
+            <img :src="img" :alt="title">
+        </div>
+        <h6 class="text-uppercase mb-5">{{title}}</h6>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Card',
+        props: {
+            img: String,
+            title: String,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.card-comics{
+    width: 200px;
+    .img-box{
+        width: 200px;
+        height: 200px;
+        overflow: hidden;
+    }
+}
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,23 +1,36 @@
 <template>
     <main>
-        <div class="container py-5"> Content Goes Here </div>
+        <div></div>
+        <div class="container py-5">
+            <div class="d-flex flex-wrap justify-content-center align-items-stretch align-content-stretch column-gap-3">
+                <div v-for="el in comicsArray">
+                    <Card :img="el.thumb" :title="el.series"/>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
 <script>
+    import Card from './Card.vue'
+    import {Comics} from '../data/dc-comics';
     export default {
-        name: 'MainComponent'
+        name: 'MainComponent',
+        components:{
+            Card
+        },
+        data(){
+            return{
+                comicsArray: Comics,
+            }
+        }
     }
 </script>
 
 <style scoped lang="scss">
 main {
-    color: white;
-    background-color: black;
-    div {
-        font-size: 2em;
-        font-weight: bold;
-    }
+    color: var(--color-light);
+    background-color: var(--color-dark);
 }
    
 </style>
